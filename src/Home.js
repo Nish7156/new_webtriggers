dd6ba2d76341d1f130e1fcf51dc4cb6fa00008bb
Your Name <your.email@example.com>
2024-04-29 05:37:25 +0530

    seo

--- src/Home.js
+++ src/Home.js
@@ -11,12 +11,24 @@ import ContactUs from "./components/Pages/Home/ContactUs";
 import MovingText from "./components/Pages/Home/MovingText";
 import Feat from "./components/Pages/Home/Feat";
 import MovingYearText from "./components/Pages/Home/MovingYearText";
+import { Helmet } from "react-helmet";
 
 function Home() {
   return (
     <>
+      <Helmet>
+        <title>Ideas Become Websites</title>
+        <meta
+          name="description"
+          content="Web development is the process of creating and maintaining websites or web applications, involving tasks such as web design, coding, and server management to ensure optimal functionality and user experience."
+        />
+        <meta
+          name="keywords"
+          content="web development,Website design,HTML, programming, coding, front-end, back-end, responsive design"
+        />
+      </Helmet>
       <HeroSection />
-      <MovingYearText/>
+      <MovingYearText />
       <AboutSection />
       <hr class="mt-0 mb-0 white" />
       <Projects />
